aca04b912da99227ccb6fb3876e317136b7e76e6
Luiz Fernando Franzon <luiz.franzon@linkedrh.com.br>
2024-10-07 17:02:12 -0300

    setup navigation-menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { NavigationMenuComponent } from "./components/navigation-menu/navigation-menu.component";
+import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, NavigationMenuComponent, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonMenuButton, IonMenu],
 })
 export class AppComponent {
   constructor() {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,13 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
   {
-    path: 'teste',
-    loadComponent: () => import('./teste/teste.page').then( m => m.TestePage)
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: 'list',
