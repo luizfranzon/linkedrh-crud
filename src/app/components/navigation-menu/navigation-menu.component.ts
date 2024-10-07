44354065f8c39da307bb5597d2fc6fa962870d96
Luiz Fernando Franzon <luiz.franzon@linkedrh.com.br>
2024-10-07 17:18:15 -0300

    remove title section from pages

--- src/app/components/navigation-menu/navigation-menu.component.ts
+++ src/app/components/navigation-menu/navigation-menu.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar } from "@ionic/angular/standalone";
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonMenuToggle, IonTitle, IonToolbar, IonList, IonItem } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-navigation-menu',
   templateUrl: './navigation-menu.component.html',
   styleUrls: ['./navigation-menu.component.scss'],
   standalone: true,
-  imports: [IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonMenuButton, IonMenu]
+  imports: [IonItem, IonList, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonMenuButton, IonMenu, IonMenuToggle, CommonModule, RouterLink]
 })
 export class NavigationMenuComponent  implements OnInit {
 
+  private router = inject(Router);
+
+  readonly navigationItems = [
+    { title: 'Home', path: '/home' }, 
+    { title: 'List', path: '/list' }, 
+    { title: 'Form', path: '/form' }
+  ];
+
+  public currentTitle = 'Home';
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
 }
